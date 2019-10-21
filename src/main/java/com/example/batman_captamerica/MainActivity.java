e2808208b9622c5babba78fb932236f468aebe0a
vladimirKartyshov <vovan198080@inbox.ru>
2019-10-21 14:03:11 +0300

    animation change

--- src/main/java/com/example/batman_captamerica/MainActivity.java
+++ src/main/java/com/example/batman_captamerica/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.batman_captamerica;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isBatmanVisible = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+                    // чтобы анимации при нажатии менялись назад и вперед
+    public void eraseBatman(View view) {
+
+        if (isBatmanVisible){
+
+            ImageView batmanImageView = findViewById(R.id.batmanImageView2);
+            batmanImageView.animate().alpha(0).setDuration(3000);
+
+            ImageView captanImageView = findViewById(R.id.captanImageView2);
+            captanImageView.animate().alpha(1).setDuration(3000);
+
+            isBatmanVisible = false;
+        }else {
+            ImageView batmanImageView = findViewById(R.id.batmanImageView2);
+            batmanImageView.animate().alpha(1).setDuration(3000);
+
+            ImageView captanImageView = findViewById(R.id.captanImageView2);
+            captanImageView.animate().alpha(0).setDuration(3000);
+
+            isBatmanVisible = true;
+        }
     }
 }
