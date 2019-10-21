221adf2482d030ba070d58726b692754f300eddc
vladimirKartyshov <vovan198080@inbox.ru>
2019-10-21 17:40:25 +0300

    rotation and scayle

--- src/main/java/com/example/batman_captamerica/MainActivity.java
+++ src/main/java/com/example/batman_captamerica/MainActivity.java
@@ -15,24 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-                    // чтобы анимации при нажатии менялись назад и вперед
+    // чтобы анимации при нажатии менялись назад и вперед и крутились и смещались по осям
     public void eraseBatman(View view) {
 
         if (isBatmanVisible){
-
             ImageView batmanImageView = findViewById(R.id.batmanImageView2);
-            batmanImageView.animate().alpha(0).setDuration(3000);
+            batmanImageView.animate().alpha(0).rotation(batmanImageView.getRotation() + 3600)
+                    .scaleY(0).scaleX(0).setDuration(3000);
 
             ImageView captanImageView = findViewById(R.id.captanImageView2);
-            captanImageView.animate().alpha(1).setDuration(3000);
+            captanImageView.animate().alpha(1).rotation(captanImageView.getRotation() + 3600)
+                    .scaleY(1).scaleX(1).setDuration(3000);
 
             isBatmanVisible = false;
         }else {
             ImageView batmanImageView = findViewById(R.id.batmanImageView2);
-            batmanImageView.animate().alpha(1).setDuration(3000);
+            batmanImageView.animate().alpha(1).rotation(batmanImageView.getRotation() + 3600)
+                    .scaleY(1).scaleX(1).setDuration(3000);
 
             ImageView captanImageView = findViewById(R.id.captanImageView2);
-            captanImageView.animate().alpha(0).setDuration(3000);
+            captanImageView.animate().alpha(0).rotation(captanImageView.getRotation() + 3600)
+                    .scaleY(0).scaleX(0).setDuration(3000);
 
             isBatmanVisible = true;
         }
